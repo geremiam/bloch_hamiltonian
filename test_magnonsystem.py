7c2dcc92a64425dafb9cc3095999116531921e7d
Geremia Massarelli <geremiam@users.noreply.github.com>
2021-12-05 18:29:33 -0500

    More quantitative tests done, this time on FMs and AFMs on square lattices in 1D and 2D. Same factor of 2 persists.
    The code was tested against the results in Ian Affleck's Les Houches lecture notes. Again, my results are half of his results.

--- test_magnonsystem.py
+++ test_magnonsystem.py
@@ -19,7 +19,18 @@ def test_1D_FM():
     
     magsys.show()
     
+    print('\n*** Energy at a single momentum ***')
+    k = [0.1]
+    ham, tau3 = magsys.bloch_ham(k, mode='RL')
+    print(f'{magsys.spin_magnitudes = }')
+    print(f'{k = }')
+    print(f'{tau3.shape = }')
+    print(f'{ham.shape = }')
+    print(f'{(tau3 @ ham).shape = }')
+    energy = np.linalg.eigvals(tau3 @ ham)
+    print(f'{energy = }')
     
+    print('\n*** Plotting band structure ***')
     k = np.linspace(-1/2., 1/2., num=500)
     
     ham, tau3 = magsys.bloch_ham(k, 'RL')
@@ -119,14 +130,25 @@ def test_1D_AFM():
 
     magsys = bh.magnonsystem_t(dim, spin_magnitudes, sl_rotations)
 
-    magsys.add_field([0,1], [2.,0,0])
+#     magsys.add_field([0,1], [2.,0,0])
 
     magsys.add_coupling((0,), 1, 0, heisen=1.)
     magsys.add_coupling((1,), 0, 1, heisen=1.)
     
     magsys.show()
     
+    print('\n*** Energy at a single momentum ***')
+    k = [0.1]
+    ham, tau3 = magsys.bloch_ham(k, mode='RL')
+    print(f'{magsys.spin_magnitudes = }')
+    print(f'{k = }')
+    print(f'{tau3.shape = }')
+    print(f'{ham.shape = }')
+    print(f'{(tau3 @ ham).shape = }')
+    energy = np.linalg.eigvals(tau3 @ ham)
+    print(f'{energy = }')
     
+    print('\n*** Plotting band structure ***')
     k = np.linspace(-1/2., 1/2., num=500)
     
     ham, tau3 = magsys.bloch_ham(k, 'RL')
@@ -139,6 +161,53 @@ def test_1D_AFM():
     ax.plot(k, energies.imag)
     plt.show()
     
+    return
+
+def test_2D_FM():
+    
+    dim = 2
+    # One spin per magnetic unit cell
+    sl_rotations = [Rotation.identity()]
+    spin_magnitudes = [1.]
+
+    magsys = bh.magnonsystem_t(dim, spin_magnitudes, sl_rotations)
+
+    magsys.add_coupling((1,0), 0, 0, heisen=-1.)
+    magsys.add_coupling((0,1), 0, 0, heisen=-1.)
+    
+    print( 'Classical energy: {}'.format(magsys.classical_energy()) )
+    
+    magsys.show()
+    
+    print('\n*** Energy at a single momentum ***')
+    k = [0.12, 0.23]
+    ham, tau3 = magsys.bloch_ham(k, mode='RL')
+    print(f'{magsys.spin_magnitudes = }')
+    print(f'{k = }')
+    print(f'{tau3.shape = }')
+    print(f'{ham.shape = }')
+    print(f'{(tau3 @ ham).shape = }')
+    energy = np.linalg.eigvals(tau3 @ ham)
+    print(f'{energy = }')
+    
+    print('\n*** Plotting band structure ***')
+    k0 = np.linspace(-0.5, 0.5, num=200)
+    k1 = np.linspace(-0.5, 0.5, num=300)
+    k = np.meshgrid(k0, k1, indexing='ij')
+    
+    ham, tau3 = magsys.bloch_ham(k, mode='RL')
+    print(f'{ham.shape = }')
+    
+    energies = np.linalg.eigvals(tau3 @ ham)
+    energies = np.sort(energies, axis=-1)
+    print(f'{energies.shape = }')
+    
+    fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+    
+    for i in range(energies.shape[-1]):
+        ax.plot_surface(k[0], k[1], energies.real[...,i])
+    plt.show()
+        
     return
 
 def test_2D_AFM_GS():
@@ -172,7 +241,7 @@ def test_2D_AFM_GS():
 
 def test_2D_AFM():
     dim = 2
-    
+    # Two spins per magnetic unit cell
     r0 = Rotation.identity()
     r1 = Rotation.from_rotvec(np.pi * np.array([0,1,0]))
     sl_rotations = [r0, r1]
@@ -187,6 +256,22 @@ def test_2D_AFM():
     magsys.add_coupling((1,1), 0, 1, heisen=1.)
     
     magsys.show()
+    
+    # Watch out! The primitive lattice vectors have been changed by the magnetic order.
+    lattice_vectors = [[1, -1], [1,1]]
+    
+    print('\n*** Energy at a single momentum ***')
+    k = [1., 1.]
+    ham, tau3 = magsys.bloch_ham(k, mode='cartesian', lattice_vectors=lattice_vectors)
+    print(f'{magsys.spin_magnitudes = }')
+    print(f'{k = }')
+    print(f'{tau3.shape = }')
+    print(f'{ham.shape = }')
+    print(f'{(tau3 @ ham).shape = }')
+    energy = np.linalg.eigvals(tau3 @ ham)
+    print(f'{energy = }')
+    
+    print('\n*** Plotting band structure ***')
     
     k0 = np.linspace(-0.5, 0.5, num=200)
     k1 = np.linspace(-0.5, 0.5, num=300)
@@ -314,4 +399,4 @@ if __name__ == '__main__':
     parser.epilog = 'Example usage: python3 test_magnonsystem.py'
     args = parser.parse_args()
     np.set_printoptions(linewidth=250)
-    test_honeycomb_AFM()
+    test_2D_AFM()
