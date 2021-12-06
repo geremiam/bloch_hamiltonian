Found the problematic factor of 2: it was in my math reasoning, rather than in the implementation. Also fixed another mistake.
The issue was with the "symmetrized" interaction tensors: I shouldn't have been halving them in my code, since I was carrying a factor of 1/2 in my notes.

I had also forgotten a factor of 1/2 in the classical energy, so I was accidentally getting the right classical energy despite the above mistake.

bloch_hamiltonian.py
@@ -204,8 +204,8 @@ class magnonsystem_t:
         
         # Symmetrize couplings, such that Jtensor_ji = Jtensor_ij^T
         tup_rev = (tuple(-np.array(R)), sl2, sl1)
-        self.couplings_sym[tup]     = Jtensor   / 2.
-        self.couplings_sym[tup_rev] = Jtensor.T / 2.
+        self.couplings_sym[tup]     = Jtensor
+        self.couplings_sym[tup_rev] = Jtensor.T
         
         for t in [tup, tup_rev]:
             # Compute the couplings as seen in the local bases
@@ -322,9 +322,9 @@ class magnonsystem_t:
     
     def classical_energy(self):
         '''
-        Computes the classical energy (order S^2 terms).
+        Computes the classical energy per magnetic unit cell (order S^2 terms).
         
-        return: The classical energy (a float).
+        return: The classical energy per magnetic unit cell (a float).
         '''
         accumulator = 0.
         
@@ -333,7 +333,7 @@ class magnonsystem_t:
             sl1 = tup[1]
             sl2 = tup[2]
             Jtilde_zz = self.couplings_sym_rot[tup][2,2]
-            accumulator += self.spin_magnitudes[sl1] * Jtilde_zz * self.spin_magnitudes[sl2]
+            accumulator += 0.5 * self.spin_magnitudes[sl1] * Jtilde_zz * self.spin_magnitudes[sl2]
         
         # Contribution from Zeeman fields
         for sl in self.fields_rot:

test_magnonsystem.py
@@ -297,7 +297,7 @@ def test_honeycomb_FM():
     r0 = Rotation.identity()
     sl_rotations = [r0, r0]
 
-    spin_magnitudes = [1., 1.]
+    spin_magnitudes = [0.5, 1.]
 
     magsys = bh.magnonsystem_t(dim, spin_magnitudes, sl_rotations)
 
@@ -348,7 +348,7 @@ def test_honeycomb_AFM():
     r1 = Rotation.from_rotvec(np.pi * np.array([0,1,0]))
     sl_rotations = [r0, r1]
 
-    spin_magnitudes = [2., 3.]
+    spin_magnitudes = [1., 1.5]
 
     magsys = bh.magnonsystem_t(dim, spin_magnitudes, sl_rotations)
 
@@ -399,4 +399,4 @@ if __name__ == '__main__':
     parser.epilog = 'Example usage: python3 test_magnonsystem.py'
     args = parser.parse_args()
     np.set_printoptions(linewidth=250)
-    test_2D_AFM()
+    test_honeycomb_FM()